aadd5a0d6fbb6edaaeb0187569a00682e8632080
weiweixiaooki <131157187+weiweixiaooki@users.noreply.github.com>
2024-05-07 14:11:07 +0800

    Add files via upload

--- university-map/src/views/ListView.vue
+++ university-map/src/views/ListView.vue
@@ -2,5 +2,29 @@
 </script>
 
 <template>
-    <div>List</div>
+<div class="nav">
+    <el-menu
+    :default-active="this.$route.path"
+    mode="horizontal"
+    :ellipsis="false"
+    router
+       >
+      <el-menu-item index="/UniversityTab">
+         <i class="el-icon-menu"></i>
+         <span>双一流高校</span>
+      </el-menu-item>
+      <el-menu-item index="/DisciplineTab">
+         <i class="el-icon-menu"></i>
+         <span>双一流专业</span>
+      </el-menu-item>
+      <el-menu-item index="/AreaTab">
+         <i class="el-icon-menu"></i>
+         <span >省市</span>
+      </el-menu-item>
+</el-menu>
+
+</div>
 </template>
+<style scoped>
+
+</style>
