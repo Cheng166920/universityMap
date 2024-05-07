7424816ad1674073ab81c5a8cea58dcbe54c5fc5
weiweixiaooki <131157187+weiweixiaooki@users.noreply.github.com>
2024-05-07 14:24:56 +0800

    Add files via upload

--- university-map/vite.config.js
+++ university-map/vite.config.js
@@ -4,8 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
-//import styleImport from 'vite-plugin-style-import'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,7 +15,6 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-   
   ],
   resolve: {
     alias: {
@@ -26,20 +24,10 @@ export default defineConfig({
   server: { // 中转服务器
     proxy: { // 通过代理实现跨域,搭理这个地址http://localhost:8081
       '/api': {  // 路径, 作为就是替换域名
-        target: 'http://127.0.0.1:3000', // 表示要替换的服务端地址
+        target: 'http://localhost:3000', // 表示要替换的服务端地址
         changeOrigin: true, // 表示开启代理, 允许跨域请求数据
         secure: false,  // 如果是https接口，需要配置这个参数 
         rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        modifyVars: {
-          hack: `true; @import (reference) "${path.resolve('src/assets/variables.less')}";`,
-        },
       }
     }
   }
